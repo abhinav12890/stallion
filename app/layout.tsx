c1904f52f727edd805625f32abd7b8e10cbf5b94
Abhinav Kumar <abhinavkumarpal890@gmail.com>
2023-09-19 22:11:37 +0530

    updated logo img search commit2

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import SEO from "@/components/SEO";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
+            <SEO/>
             <Header />
             {children}
             <Footer />
